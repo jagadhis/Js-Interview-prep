d08106a568f32650c09e8866dfd081a642799867
jagadhis_0220 <jwaran78@gmail.com>
2022-01-17 13:50:39 +0530

    commit

--- script.js
+++ script.js
@@ -13,3 +13,59 @@ var hello = () => {
 }
 
 hello();
+
+const o = {
+"BreadOmlette": {
+    "bread": 2,
+    "eggs": 5,
+    "onion": 5,
+    "tomato": 2,
+    "salt": 10
+  },
+  "Cake": {
+    "eggs": 2,
+    "flour": 1,
+    "butter": 2,
+    "sugar": 4
+  },
+  "FrenchToast": {
+    "eggs": 3,
+    "salt": 1,
+    "milk": 1,
+    "bread": 4,
+    "flour": 1,
+    "sugar": 4,
+    "butter": 1
+  },
+  "Pudding": {
+    "milk": 3,
+    "flour": 5,
+    "sugar": 4
+  },
+  "Sandwich": {
+    "onion": 3,
+    "tomato": 2,
+    "lettuce": 3,
+    "bread": 2
+  }
+}
+
+function sum() {
+const counter = {}
+for(let x in o){
+    let y = o[x];
+    for(let itr in y){
+        console.log(itr);
+        if(counter[itr] === undefined){
+            counter[itr] = y[itr];
+        }
+        else{
+            counter[itr] += y[itr];
+
+        }
+    }
+}
+console.log(counter);
+}
+
+sum();
